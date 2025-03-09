c5dc6fb2336af84d16b75d8412bd6362b3b29c7c
justin-onyango <ojustin446@gmail.com>
2025-03-09 23:06:20 +0300

    Init string reverse

--- mysite/mysite/__init__.py
+++ mysite/mysite/__init__.py
@@ -8,3 +8,11 @@ class StringReverserTestCase(TestCase):
     def test_reverse_string(self):
         reverser = StringReverser()
         self.assertEqual(reverser.reverse_string('hello'), 'olleh')
+
+
+class StringReverserTest(TestCase):
+    def test_reverse_string(self):
+        reverser = StringReverser()
+        self.assertEqual(reverser.reverse_string('hello'), 'olleh')
+
+
